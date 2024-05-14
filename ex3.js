d7c591d7d5fc8d6a76ee2456fd615f13c9017dac
patchanaab <165882679+patchanaab@users.noreply.github.com>
2024-05-14 08:57:54 +0700

    exercise3

---
+++ ex3.js
@@ -0,0 +1,9 @@
+let dayNumber = 6;
+
+if(dayNumber === 0 || dayNumber === 6){
+        console.log("It's a Weekends!");
+}else if (dayNumber >=1  && dayNumber <= 5 ) {
+    console.log("It is the weekday!");
+}else{
+        console.log("There are only seven days");
+}
